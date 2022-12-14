05d0f66593ca7536f0078a3ad242d0e4c5f824eb
anhbv <anhbuidustin@gmail.com>
2022-12-14 16:53:20 +0900

    update

--- BlazorWebAppBase/Program.cs
+++ BlazorWebAppBase/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
